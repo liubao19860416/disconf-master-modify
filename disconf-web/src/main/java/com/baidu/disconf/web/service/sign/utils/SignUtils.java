ad3ee3fef6e64d404d6b95f8e1a25fab9d574fab
liubao <18611478781@163.com>
2016-12-22 15:27:36 +0800

    天津唉测试类

--- disconf-web/src/main/java/com/baidu/disconf/web/service/sign/utils/SignUtils.java
+++ disconf-web/src/main/java/com/baidu/disconf/web/service/sign/utils/SignUtils.java
@@ -4,28 +4,34 @@ import java.util.UUID;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
+@SuppressWarnings("deprecation")
 public class SignUtils {
+	
+	/**
+	 * 密码为:8338f82cbd01051fc14a0f0962ebc1848adaf47b
+	 * Token为:3d86cc6a444504e52adb462a8e39c28937676afc
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String userName="liubao";
+		String password="liubao";
+		System.out.println("密码为:"+createPassword(password));
+		System.out.println("Token为:"+createToken(userName));
+	}
 
     /**
      * 生成密码， 使用shaHex加密
-     *
-     * @return
      */
-    public static String createPassword(String password) {
-
+	public static String createPassword(String password) {
         String data = DigestUtils.shaHex(password);
         return data;
     }
 
     /**
      * 生成token ,使用 UUID + 手机生成
-     *
-     * @return
      */
     public static String createToken(String phone) {
-
         String uuid = UUID.randomUUID().toString();
-
         String data = DigestUtils.shaHex(uuid + phone);
         return data;
     }
